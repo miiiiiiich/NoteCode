2ed189f261719b2904b7d3e238d177b74d2f948c
Michisato <Michisato.Hero@gmail.com>
2020-06-01 15:51:35 +0900

    コードを少しみやすくして完了

--- dunning_kruger.R
+++ dunning_kruger.R
@@ -5,17 +5,16 @@
 library(tidyverse)
 competence <- 0:96
 confidence <- NULL
-for (i in competence){
+for (i in competence) {
   confidence <- c(confidence,
-                  if_else(i <= 10, true = i * 8.4, false = 1/28 * (i - 51)^2 + 20))
+                  if_else(i <= 10, true = i * 8.4, false = 1 / 28 * (i - 51)^2 + 20))
 }
 g <- ggplot(data = NULL, aes(x = competence, y = confidence)) +
-  geom_line(color = "red", size = 2, alpha = 0.4) +
-  geom_hline(yintercept = 0, size=1) +
-  geom_vline(xintercept = 0, size=1) +
+  geom_line(color = "red", size = 2, alphl = 0.4) +
+  geom_hline(yintercept = 0, size = 1) +
+  geom_vline(xintercept = 0, size = 1) +
   ggtitle("Dunning-Kruger Effect") +
   theme(plot.title = element_text(hjust = 0.5, size = 30)) +
-  annotate("text", x=10,   y=88, label="Begginer", size=5) +
-  annotate("text", x=96, y=80, label="Expert", size = 5)
-
+  annotate("text", x = 10, y = 88, label = "Begginer", size = 5) +
+  annotate("text", x = 96, y = 80, label = "Expert", size = 5)
 g
