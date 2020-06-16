10255bfae4b035b9ea6c056ba368cc05658b961f
Michisato <Michisato.Hero@gmail.com>
2020-06-16 16:13:43 +0900

    進化心理学のコード完成

--- evolutionary_psychology.py
+++ evolutionary_psychology.py
@@ -1,9 +1,9 @@
 class Mammalian:
     def __init__(self, name, gender="man"):
-        self.name = name
+        self.name = name + "くん" if gender == "man" else name + "ちゃん"
         self.gender = gender
         self.recognize = True
-        self.period = False
+        self.period = False if self.gender == "woman" else True
         assert self.gender == "man" or self.gender == "woman", "性別が不明です。"
 
     def match_gender(self, opponent):
@@ -19,9 +19,12 @@ class Mammalian:
             return False
 
     def breeding_season(self, breeding=True):
-        self.period = breeding
+        self.period = breeding if self.gender == "woman" else True
         assert type(breeding) == bool
         print("繁殖期です。") if breeding else print("繁殖期ではないです。")
+
+    def get_recognize_breeding(self):
+        return self.recognize
 
 
 class Monkey(Mammalian):
@@ -36,28 +39,33 @@ class Monkey(Mammalian):
 
 
 class Human(Mammalian):
+    def __init__(self, name, gender="man"):
+        super().__init__(name, gender)
+        self.recognize = False
+
     def meet(self, opponent):
-        if type(self) == type(self):
+        if type(self) == type(opponent):
             if self.match_gender(opponent) and self.match_period(opponent):
-                print("{0}と{1}は子供ができる。".format(self.name, opponent.name))
+                print("{0}と{1}は子のができる。".format(self.name, opponent.name))
             elif self.match_gender(opponent):
-                print("{0}と{1}は性行為をするが子供は生まれない。")
+                print("{0}と{1}は性行為をするが子供は生まれない。".format(self.name, opponent.name))
             else:
                 print("{0}と{1}は友達の可能性は高い。".format(self.name, opponent.name))
 
         else:
-            print("種族が違います。")
+            print("{0}と{1}種族が違います。".format(self.name, opponent.name))
 
 
 if __name__ == '__main__':
-    monkey = Monkey(name="bono子", gender="woman")
-    monkey_1 = Monkey(name="bonobo", gender="man")
+    monkey = Monkey(name="ボノ子", gender="woman")
+    monkey_1 = Monkey(name="ボノボ", gender="man")
     iam = Human(name="牛乳", gender="man")
     she = Human(name="Japan", gender="woman")
     monkey.breeding_season()
     monkey.meet(monkey_1)
     monkey_1.breeding_season()
     monkey.meet(monkey_1)
-    print(monkey.period)
     iam.meet(she)
     iam.meet(monkey)
+    she.breeding_season()
+    she.meet(iam)
